0ed13df20712440119bfd2fbd26336621b72ea17
Antaryami Behera <antaryamibehera2003@gmail.com>
2024-10-02 14:14:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const secondHand = document.getElementById('second-hand');
+const minuteHand = document.getElementById('minute-hand');
+const hourHand = document.getElementById('hour-hand');
+
+function clockticks()
+{
+    const date = new Date();
+    const second = date.getSeconds() /60;
+    const minute = (second + date.getMinutes()) /60;
+    const hours = (minute + date.getHours()) /12;
+
+    rotateClockHand(secondHand, second);
+    rotateClockHand(minuteHand, minute);
+    rotateClockHand(hourHand, hours);
+}
+
+function rotateClockHand(element, rotation){
+    element.style.setProperty('--rotate', rotation * 360);
+}
+setInterval(clockticks, 1000);
